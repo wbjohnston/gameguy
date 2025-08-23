eddb70ba9e9c5cc98629e0996171f7517f3c8dfc
Will Johnston <wbjohnston@gmail.com>
2025-08-23 12:42:38 -0700

    basic CLI

--- src/bin/gbdb.rs
+++ src/bin/gbdb.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+use std::path::Path;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    rom_path: String,
+}
+
 fn main() {
-    println!("hello world")
+    let args = Args::parse();
+    dbg!(&args);
 }
